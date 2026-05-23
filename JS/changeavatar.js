1a3b2929f43c2ba29f5d357f892abb0945e46baa
danh3426 <danhb2408063@student.ctu.edu.vn>
2026-05-23 18:07:12 +0700

    push

---
+++ JS/changeavatar.js
@@ -0,0 +1,48 @@
+function finduser(signinusername, signinpassword, userlist) {
+  if (userlist.length === 0) return false;
+  else {
+    for (let i = 0; i < userlist.length; i++) {
+      if (
+        signinusername === userlist[i].username &&
+        signinpassword === userlist[i].password
+      )
+        return userlist[i];
+    }
+    return false;
+  }
+}
+function changeavatar(event) {
+  const file = event.target.files[0];
+  if (!file) return;
+  const useravatar = new FileReader();
+
+  const userlist = JSON.parse(window.localStorage.getItem("userlist")) || [];
+  const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
+  const userData = finduser(currentUser, userlist);
+
+  useravatar.onload = function (e) {
+    const img = new Image();
+    img.src = e.target.result;
+
+    img.onload = function () {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      const size = 150;
+      canvas.width = size;
+      canvas.height = size;
+      ctx.drawImage(img, 0, 0, size, size);
+      const imgURL = canvas.toDataURL("image/jpeg", 0.7);
+
+      if (currentUser) {
+        currentUser.avatar = imgURL;
+        userData.avatar = imgURL;
+      }
+      window.localStorage.setItem("userlist", JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+
+      const imageavatar = document.getElementById("avatar");
+      imageavatar.src = imgURL;
+    };
+  };
+  useravatar.readAsDataURL(file);
+}
---
+++ JS/updatedinformation.js
@@ -0,0 +1,349 @@
+function checkFullName(fullname) {
+  return /^[\p{L}\s]+$/u.test(fullname);
+}
+function checkUserName(username) {
+  return /^[\p{L}\p{Nd}\s]+$/u.test(username);
+}
+function checkphonenumber(phonenumber) {
+  return /^[\p{Nd}]+$/u.test(phonenumber);
+}
+function updatedbutton() {
+  const fullnamebutton = document.getElementById("changename");
+  const usernamebutton = document.getElementById("changeusername");
+  const phonenumberbutton = document.getElementById("changephonenumber");
+  const emailbutton = document.getElementById("changeemail");
+  const birthbutton = document.getElementById("changebirthday");
+  const sexbutton = document.getElementById("changesex");
+  const welcomeuser = document.getElementById("username");
+
+  const updatedname = document.getElementsByClassName(
+    "change-information__fullname",
+  )[0];
+  const updatedusername = document.getElementsByClassName(
+    "change-information__username",
+  )[0];
+  const updatedphone = document.getElementsByClassName(
+    "change-information__phonenumber",
+  )[0];
+  const updatedemail = document.getElementsByClassName(
+    "change-information__email",
+  )[0];
+  const updatedbirth = document.getElementsByClassName(
+    "change-information__birthday",
+  )[0];
+  const updatedsex = document.getElementsByClassName(
+    "change-information__sex",
+  )[0];
+
+  const fullnameclosebutton = document.getElementById(
+    "change-information__fullname--close",
+  );
+  const usernameclosebutton = document.getElementById(
+    "change-information__username--close",
+  );
+  const phonenumberclosebutton = document.getElementById(
+    "change-information__phone--close",
+  );
+  const emailclosebutton = document.getElementById(
+    "change-information__email--close",
+  );
+  const birthdayclosebutton = document.getElementById(
+    "change-information__birthday--close",
+  );
+  const sexclosebutton = document.getElementById(
+    "change-information__sex--close",
+  );
+  function appear(element) {
+    element.style.display = "flex";
+  }
+  fullnamebutton.addEventListener("click", () => appear(updatedname));
+  usernamebutton.addEventListener("click", () => appear(updatedusername));
+  phonenumberbutton.addEventListener("click", () => appear(updatedphone));
+  emailbutton.addEventListener("click", () => appear(updatedemail));
+  birthbutton.addEventListener("click", () => appear(updatedbirth));
+  sexbutton.addEventListener("click", () => appear(updatedsex));
+}
+document.addEventListener("DOMContentLoaded", updatedbutton);
+function closeupdatedbutton() {
+  const fullnamebutton = document.getElementById("changename");
+  const usernamebutton = document.getElementById("changeusername");
+  const phonenumberbutton = document.getElementById("changephonenumber");
+  const emailbutton = document.getElementById("changeemail");
+  const birthbutton = document.getElementById("changebirthday");
+  const sexbutton = document.getElementById("changesex");
+
+  const updatedname = document.getElementsByClassName(
+    "change-information__fullname",
+  )[0];
+  const updatedusername = document.getElementsByClassName(
+    "change-information__username",
+  )[0];
+  const updatedphone = document.getElementsByClassName(
+    "change-information__phonenumber",
+  )[0];
+  const updatedemail = document.getElementsByClassName(
+    "change-information__email",
+  )[0];
+  const updatedbirth = document.getElementsByClassName(
+    "change-information__birthday",
+  )[0];
+  const updatedsex = document.getElementsByClassName(
+    "change-information__sex",
+  )[0];
+
+  const fullnameclosebutton = document.getElementById(
+    "change-information__fullname--close",
+  );
+  const usernameclosebutton = document.getElementById(
+    "change-information__username--close",
+  );
+  const phonenumberclosebutton = document.getElementById(
+    "change-information__phonenumber--close",
+  );
+  const emailclosebutton = document.getElementById(
+    "change-information__email--close",
+  );
+  const birthdayclosebutton = document.getElementById(
+    "change-information__birthday--close",
+  );
+  const sexclosebutton = document.getElementById(
+    "change-information__sex--close",
+  );
+  function close(element) {
+    element.style.display = "none";
+  }
+  fullnameclosebutton.addEventListener("click", () => close(updatedname));
+  usernameclosebutton.addEventListener("click", () => close(updatedusername));
+  phonenumberclosebutton.addEventListener("click", () => close(updatedphone));
+  emailclosebutton.addEventListener("click", () => close(updatedemail));
+  birthdayclosebutton.addEventListener("click", () => close(updatedbirth));
+  sexclosebutton.addEventListener("click", () => close(updatedsex));
+}
+document.addEventListener("DOMContentLoaded", closeupdatedbutton);
+
+function finduserinuserlist(currentUser, userlist) {
+  for (let i = 0; i < userlist.length; i++) {
+    if (
+      currentUser.fullname === userlist[i].fullname &&
+      currentUser.username === userlist[i].username &&
+      currentUser.password === userlist[i].password
+    ) {
+      return userlist[i];
+    }
+  }
+  return null;
+}
+function updatedinformation() {
+  const userlist = JSON.parse(window.localStorage.getItem("userlist")) || [];
+  const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
+  const userData = finduserinuserlist(currentUser, userlist);
+
+  const avatar = document.getElementById("avatar");
+  const fullname = document.getElementById("user-information__name--fullname");
+  const username = document.getElementById(
+    "user-information__username--username",
+  );
+  const welcomeuser = document.getElementById("username");
+  const phonenumber = document.getElementById(
+    "user-information__phone--phonenumber",
+  );
+  const email = document.getElementById("user-information__email--mail");
+  const birthday = document.getElementById("user-information__birth--birthday");
+  const sex = document.getElementById("user-information__sex--sex");
+
+  const updatedfullname = document.getElementById(
+    "change-information__fullname",
+  );
+  const updatedusername = document.getElementById(
+    "change-information__username",
+  );
+  const updatedphonenumber = document.getElementById(
+    "change-information__phonenumber",
+  );
+  const updatedemail = document.getElementById("change-information__email");
+  const updatedbirthday = document.getElementById(
+    "change-information__birthday",
+  );
+  const updatedsex = document.getElementById("change-information__sex");
+
+  const fullnameagreebutton = document.getElementById(
+    "change-information__fullname--agree",
+  );
+  const usernameagreebutton = document.getElementById(
+    "change-information__username--agree",
+  );
+  const phonenumberagreebutton = document.getElementById(
+    "change-information__phonenumber--agree",
+  );
+  const emailagreebutton = document.getElementById(
+    "change-information__email--agree",
+  );
+  const birthdayagreebutton = document.getElementById(
+    "change-information__birthday--agree",
+  );
+  const sexagreebutton = document.getElementById(
+    "change-information__sex--agree",
+  );
+
+  const updatedfullnamearea = document.getElementsByClassName(
+    "change-information__fullname",
+  )[0];
+  const updatedusernamearea = document.getElementsByClassName(
+    "change-information__username",
+  )[0];
+  const updatedphonenumberarea = document.getElementsByClassName(
+    "change-information__phonenumber",
+  )[0];
+  const updatedemailarea = document.getElementsByClassName(
+    "change-information__email",
+  )[0];
+  const updatedbirthdayarea = document.getElementsByClassName(
+    "change-information__birthday",
+  )[0];
+  const updatedsexarea = document.getElementsByClassName(
+    "change-information__sex",
+  )[0];
+
+  let avatarPath = userData.avatar;
+  if (
+    !avatarPath.startsWith("data:") &&
+    window.location.pathname.includes("/page/")
+  ) {
+    avatarPath = "../" + avatarPath;
+  }
+  avatar.src = avatarPath;
+  fullname.textContent = "Họ tên: " + userData.fullname;
+  username.textContent = "Tên đăng nhập: " + userData.username;
+  welcomeuser.textContent = userData.username;
+  phonenumber.textContent = "SĐT: " + userData.phone;
+  email.textContent = "Email: " + userData.email;
+  birthday.textContent = "Ngày sinh: " + userData.birth;
+  sex.textContent = "Giới tính: " + userData.sex;
+
+  function changefullname(str) {
+    if (str === "") return;
+    else {
+      userData.fullname = str;
+      currentUser.fullname = str;
+      window.localStorage.setItem("userlist", JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+      fullname.textContent = "Họ tên: " + userData.fullname;
+      updatedfullnamearea.style.display = "none";
+      alert("Cập nhật thành công!!");
+    }
+  }
+  function changeusername(str) {
+    if (str === "") return;
+    else {
+      userData.username = str;
+      currentUser.username = str;
+      window.localStorage.setItem("userlist", JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+      username.textContent = "Tên đăng nhập: " + userData.username;
+      welcomeuser.textContent = userData.username;
+      updatedusernamearea.style.display = "none";
+      alert("Cập nhật thành công!!");
+    }
+  }
+  function changephonenumber(str) {
+    if (str === "") return;
+    else {
+      userData.phone = str;
+      currentUser.phone = str;
+      window.localStorage.setItem("userlist", JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+      phonenumber.textContent = "SĐT: " + userData.phone;
+      updatedphonenumberarea.style.display = "none";
+      alert("Cập nhật thành công!!");
+    }
+  }
+  function changeemail(str) {
+    if (str === "") return;
+    else {
+      userData.email = str;
+      currentUser.email = str;
+      window.localStorage.setItem("userlist", JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+      email.textContent = "Email: " + userData.email;
+      updatedemailarea.style.display = "none";
+      alert("Cập nhật thành công!!");
+    }
+  }
+  function changebirthday(str) {
+    if (str === "") return;
+    else {
+      userData.birth = str;
+      currentUser.birth = str;
+      window.localStorage.setItem("userlist", JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+      birthday.textContent = "Ngày sinh: " + userData.birth;
+      updatedbirthdayarea.style.display = "none";
+      alert("Cập nhật thành công!!");
+    }
+  }
+  function changesex(str) {
+    if (str === "") return;
+    else {
+      userData.sex = str;
+      currentUser.sex = str;
+      window.localStorage.setItem("userlist", JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+      sex.textContent = "Giới tính: " + userData.sex;
+      updatedsexarea.style.display = "none";
+      alert("Cập nhật thành công!!");
+    }
+  }
+  fullnameagreebutton.addEventListener("click", () =>
+    changefullname(updatedfullname.value),
+  );
+  usernameagreebutton.addEventListener("click", () =>
+    changeusername(updatedusername.value),
+  );
+  phonenumberagreebutton.addEventListener("click", () =>
+    changephonenumber(updatedphonenumber.value),
+  );
+  emailagreebutton.addEventListener("click", () =>
+    changeemail(updatedemail.value),
+  );
+  birthdayagreebutton.addEventListener("click", () =>
+    changebirthday(updatedbirthday.value),
+  );
+  sexagreebutton.addEventListener("click", () => changesex(updatedsex.value));
+}
+document.addEventListener("DOMContentLoaded", updatedinformation);
+
+function changeavatar(event) {
+  const file = event.target.files[0];
+  if (!file) return;
+  const useravatar = new FileReader();
+
+  const userlist = JSON.parse(window.localStorage.getItem("userlist")) || [];
+  const currentUser = JSON.parse(window.localStorage.getItem("currentUser"));
+  const userData = finduserinuserlist(currentUser, userlist);
+
+  useravatar.onload = function (e) {
+    const img = new Image();
+    img.src = e.target.result;
+
+    img.onload = function () {
+      const canvas = document.createElement("canvas");
+      const ctx = canvas.getContext("2d");
+      const size = 150;
+      canvas.width = size;
+      canvas.height = size;
+      ctx.drawImage(img, 0, 0, size, size);
+      const imgURL = canvas.toDataURL("image/jpeg", 0.7);
+
+      if (currentUser) {
+        currentUser.avatar = imgURL;
+        userData.avatar = imgURL;
+      }
+      window.localStorage.setItem("userlist", JSON.stringify(userlist));
+      window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+
+      const imageavatar = document.getElementById("avatar");
+      imageavatar.src = imgURL;
+    };
+  };
+  useravatar.readAsDataURL(file);
+}
